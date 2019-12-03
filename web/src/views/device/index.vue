164121ae30acc644b66660ced6f34b5edd23baf5
rk <rk@gmail.com>
2019-12-03 15:42:00 +0800

    1

--- web/src/views/device/index.vue
+++ web/src/views/device/index.vue
@@ -116,6 +116,7 @@ export default {
       })
         .then(res => {
           this.listLoading = false;
+          this.$store.dispatch('device/updateOnlineDevices');
         })
         .finally(() => {
           this.listLoading = false;
