a992ca5359249f3cc19c2984fce330e36d98ba04
rk <rk@gmail.com>
2019-12-03 10:51:26 +0800

    chore

--- web/src/layout/components/Navbar.vue
+++ web/src/layout/components/Navbar.vue
@@ -16,11 +16,11 @@
               Home
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+          <a target="_blank" href="https://github.com/zrk1993/autojs-web-control">
             <el-dropdown-item>Github</el-dropdown-item>
           </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
+          <a target="_blank" href="https://hyb1996.github.io/AutoJs-Docs/#/">
+            <el-dropdown-item>Autojs</el-dropdown-item>
           </a>
           <el-dropdown-item divided>
             <span style="display:block;" @click="logout">Log Out</span>
